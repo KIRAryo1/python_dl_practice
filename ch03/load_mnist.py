8ef9bc9d845e840da91f026ac05dfd20e3863da1
Ryoichi Kira <r.kira@grooves.co.jp>
2018-07-19 20:53:11 +0900

    forward propagation with sample weight

--- ch03/load_mnist.py
+++ ch03/load_mnist.py
@@ -1,6 +1,11 @@
 import sys, os
 sys.path.append(os.pardir)
+# sys.path.append(os.path)
+# sys.path.append(os.path.pardir)
+# print(sys.path)
+# print(os.path.exists("./dataset/mnist.py"))
 from dataset.mnist import load_mnist
+
 
 (x_train, t_train), (x_test, t_test) = \
     load_mnist(flatten=True, normalize=False)
